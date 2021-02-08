b5ffb21762a6f89c1e21082ebf22a67495b14562
sowonday <kksw5324@gmail.com>
2021-02-08 18:05:25 +0900

    Initial Commit

---
+++ SwDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwDiary
+//
+//  Created by 김소원 on 2021/02/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
